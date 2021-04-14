ac5cf40b5531c9c24143836b19cdfaf3383d3af1
ayoubkandah <a.kandah994@gmail.com>
2021-04-15 02:55:21 +0300

    GamePlay

--- index.js
+++ index.js
@@ -1,38 +1,52 @@
+'use strict'
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
-const app = express();
-const server = http.createServer(app);
+const app=express();
+const server=http.createServer(app);
 const io = require('socket.io')(http);
-const PORT =  3030;
-// const { v4: uuidv4 } = require('uuid');
-// const mongoose = require('mongoose');
-// require('dotenv').config();
+const fs =require("fs")
+// const path = require('path');
+const PORT=3030;
 let currentRoom;
 io.listen(server);
 app.use(cors());
-
-
+let client=0;
+ let trigger=true;
 io.on('connection', (socket) => {
-    console.log("client connected");
-    socket.emit("hello")
-    // socket.on("changeColor",()=>{
-currentRoom="game1"
-        
-    // })
-    socket.on("room",(value)=>{
-        socket.join(currentRoom)
+   // console.log(client,"dddd")
+let room="a"   
+   client++
+   console.log(client)
+    socket.join(room)
+
+   if(client==1){
+      console.log("in")
+      // console.log(socket.id)
+      socket.emit("waiting")
+   }else if(client==2){
+      trigger= false
+client=0
+   socket.nsp.to(room).emit("start",room)
+   
+   }else{
+   }
+   socket.on('disconnect', () => {
+   if(client>0){
+   client--;
+   }
+    });
+    socket.on("click",(value)=>{
+       
+       socket.to(room).emit("touched",value)
+    })   
+
+    socket.on("lose",()=>{
+     socket.to(room).emit("loser")
+
     })
-    socket.on("changingText",()=>{
-
-        socket.in(currentRoom).emit("changeValue")
-
-    })
-
-    socket.on("msg1",value=>{
-        console.log(value)
-socket.in(currentRoom).emit("displayMsg",value)
-
+    socket.on("draw",()=>{
+       socket.to(room).emit("playerDraw")
     })
 });
 server.listen(PORT, () => console.log('Listening on PORT ' + PORT));
