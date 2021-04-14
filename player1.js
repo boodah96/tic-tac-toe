bc052a3c284dbb51ab23a211fc0a212a58f91e89
ayoubkandah <a.kandah994@gmail.com>
2021-04-14 21:32:47 +0300

    socket

--- player1.js
+++ player1.js
@@ -4,24 +4,53 @@ const options = {
     transports: ['websocket'],
   };
   const socket = io('localhost:3030/', options); // emmit connection event to server
-
+let trigger=false;
+let room;
+$("#game").hide()
+$("#div").text("Waiting")
   const player={name:"player1"}
   socket.on("connect",()=>{
-    //   console.log("client listener")
-socket.on("hello",()=>{
-    console.log("eeee")
+  socket.on("waiting",value=>{
+    trigger=true
+  console.log("waiting")
 })
+//----------Start
+socket.on("start",(value)=>{
+  $("#game").show()
 
+  console.log("Start")
+room=value
+$("#div").text("Start")
+if(trigger){
 
-socket.emit("room",(player))
+  $("#button").attr('disabled', false);
 
-
-
-$("#form1").on("submit",(e)=>{
-    e.preventDefault()
-    let msg = e.target.player1.value
-    // console.log(e.target.player1.value)
-    socket.emit("msg1",msg)
-})
-
+}else{
+  $("#button").attr('disabled', true);
+}
+//--------When Clicked
   })
+  socket.on("touched",()=>{
+    $("#button").attr("disabled",false)
+  })
+  
+  $("#button").on("click",()=>{
+    $("#button").attr('disabled', true);
+    socket.emit("click")
+})
+    
+// $("#form1").on("submit",(e)=>{
+//   e.preventDefault()
+//   let msg = e.target.player1.value
+//   // console.log(e.target.player1.value)
+//   socket.emit("msg1",msg)
+// })
+})
+
+
+
+
+
+
+
+  
